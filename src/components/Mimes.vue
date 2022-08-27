8888b0ce9443b92628a725ffecc77436451dbe6f
Camille Fite <camille.fite@gmail.com>
2022-08-27 11:43:24 +0200

    Turn based system and scoreboard display at the end of the game

--- src/components/Mimes.vue
+++ src/components/Mimes.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container mimes">
-    <h1 class="title">Tour de {{currentPlayer.name}}</h1>
+    <h1 class="title" v-if="!displayScoreboard">Tour de {{currentPlayer.name}}</h1>
 
-    <!-- Before the game -->
-    <div class="content" v-if="!playing">
+    <!-- Before the game TODO update this -->
+    <div class="content" v-if="!playing && !displayScoreboard">
       <div class="notification is-info">
         <p><b>L'équipe bleue</b> est composée<br/>des personnes suivantes:</p>
         <div class="tags">
@@ -27,14 +27,40 @@
 
     <!-- Done ! -->
     <div class="notification is-primary" v-if="roundDone">
-      <p><b>Manche terminée!</b><br/>L'équipe {{ winner }} gagne 🥳</p><br/>
-      <button class="button is-primary is-light is-small">Manche suivante</button>
+      <p><b>{{ lastRound ? 'Partie' : 'Manche' }} terminée!</b><br/>L'équipe {{ winner }} gagne 🥳</p><br/>
+      <button class="button is-primary is-light is-small" @click="done">
+        {{ lastRound ? 'Voir les scores' : 'Manche suivante' }}
+      </button>
     </div>
 
     <!-- Currently playing -->
-    <div class="block list" v-if="playing">
+    <div class="block list" v-if="playing && !displayScoreboard">
       <CardItem colour="blue" :words="currentWordsList" :index="status.left" @next="status.left++"/>
       <CardItem colour="red" :words="currentWordsList" :index="status.right" @next="status.right++"/>
+    </div>
+
+    <div class="block" v-if="displayScoreboard">
+      <h1 class="title is-1">Scores</h1>
+      <ul>
+        <li v-for="p in players" :key="p.id" class="notification is-light" style="display:flex; justify-content:space-between;">
+          <b>{{p.name}}</b><span>{{p.score}} points</span>
+        </li>
+        <!-- TODO styling :
+        <li class="notification is-warning" style="display:flex; justify-content:space-between;">
+          <b>🏆 Camille</b><span>45 points</span>
+        </li>
+        <li class="notification is-light" style="display:flex; justify-content:space-between;">
+          <b>Constan</b><span>23 points</span>
+        </li>
+        <li class="notification is-danger" style="display:flex; justify-content:space-between;">
+          <b>Mary</b><span>10 points</span>
+        </li>
+        -->
+      </ul>
+      <hr/>
+      <a href="/">
+        <button class="button is-light">Retour</button>
+      </a>
     </div>
 
   </div>
@@ -54,12 +80,14 @@ export default {
   data: function () {
     return {
       playing: false,
+      lastRound: false,
+      displayScoreboard: false,
       players: [],
       game: null,
       currentPlayer: {name: "?"},
       currentWordsList: [],
       status: { left: 0, right: 0 },
-      admin: this.$route.query.admin
+      admin: this.$route.query.admin,
     }
   },
   components: {
@@ -67,7 +95,8 @@ export default {
   },
   computed: {
     roundDone: function () {
-      return this.status.left == this.currentWordsList.length || this.status.right == this.currentWordsList.length
+      return !this.displayScoreboard &&
+        (this.status.left == this.currentWordsList.length || this.status.right == this.currentWordsList.length)
     },
     winner: function () {
       return this.status.left == this.currentWordsList.length ? 'bleue' : 'rouge'
@@ -89,11 +118,26 @@ export default {
     });
   },
   methods: {
+    done: function () {
+      this.currentPlayer.hasPlayed = true
+      axios.put('http://localhost:3000' + `/players/${this.currentPlayer.id}`, this.currentPlayer)
+        .then(response => {
+          this.$router.go() // reload page to trigger player update
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
     decideNewPlayer: function () {
-      /* delete this vvv */
       const haveNotPlayed = this.players.filter(p => !p.hasPlayed)
+      if(haveNotPlayed.length == 1) this.lastRound = true
+      if(haveNotPlayed.length == 0) {
+        this.displayScoreboard = true
+        return
+      }
+
       this.currentPlayer = haveNotPlayed[randomInt(this.game.dateCreated/haveNotPlayed.length, haveNotPlayed.length)]
-      /* delete this ^^^ */
       this.currentWordsList = this.currentPlayer.wordsList
     }
   }
